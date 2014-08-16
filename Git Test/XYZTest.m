85f6ebbdf80ade05a3e18bb843bc10d185c644c7
Bruce Rich <brich@bruces-mbp.attlocal.net>
2014-08-15 21:44:46 -0500

    added an empty test file

---
+++ Git Test/XYZTest.m
@@ -0,0 +1,13 @@
+//
+//  XYZTest.m
+//  Git Test
+//
+//  Created by Bruce Rich on 8/15/14.
+//  Copyright (c) 2014 NoNameOrg. All rights reserved.
+//
+
+#import "XYZTest.h"
+
+@implementation XYZTest
+
+@end
